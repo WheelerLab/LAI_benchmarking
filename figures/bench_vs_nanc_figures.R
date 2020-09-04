f69e3922f40f4628e4cb7b6476cc761b59924da2
RyanSchu <rschubert1@luc.edu>
2020-09-04 16:57:09 -0500

    added facet lettering per formatting

--- figures/bench_vs_nanc_figures.R
+++ figures/bench_vs_nanc_figures.R
@@ -3,6 +3,17 @@ library(data.table)
 library(multcompView)
 library(ggplot2)
 library(viridis)
+library(egg)
+
+tag_facet2 <- function(p, open = "", close = "", tag_pool = LETTERS, x = -Inf, y = Inf, ## from stackoverflow https://stackoverflow.com/questions/56064042/using-facet-tags-and-strip-labels-together-in-ggplot2 based on egg tag_facet
+                       hjust = -0.5, vjust = 1.5, fontface = 2, family = "", ...) {
+  
+  gb <- ggplot_build(p)
+  lay <- gb$layout$layout
+  tags <- cbind(lay, label = paste0(open, tag_pool[lay$PANEL], close), x = x, y = y)
+  p + geom_text(data = tags, aes_string(x = "x", y = "y", label = "label"), ..., hjust = hjust, 
+                vjust = vjust, fontface = fontface, family = family, inherit.aes = FALSE)
+}
 
 bench<-fread("Z:/data/Local_ancestry_project/revisions/time_mem_benchmarks.txt")
 
@@ -11,12 +22,14 @@ bench<-bench %>%   mutate(mem=mem/1000)
 ##time
 ggroups<-ggplot(data = bench)
 pdf("Z:/data/Local_ancestry_project/revisions/time_chr22_new_sims.pdf")
-ggroups + geom_point(aes(x=as.factor(nanc), y =time, colour =as.factor(nanc))) + 
+f1<-ggroups + geom_point(aes(x=as.factor(nanc), y =time, colour =as.factor(nanc))) + 
   facet_wrap(~software) +
   theme_bw(15) +
   labs(colour= "Number of \nAncestral \nPopulations") +
   xlab("Number of Ancestral Populations") +
-  ylab("Runtime (s)")
+  ylab("Runtime (s)") +
+  theme(legend.position = "none")
+tag_facet2(f1)
 dev.off()
 
 for (i in c("RFMix","ELAI","LAMPLD","MOSAIC","Loter")){
@@ -27,12 +40,14 @@ for (i in c("RFMix","ELAI","LAMPLD","MOSAIC","Loter")){
 }
 
 pdf("Z:/data/Local_ancestry_project/revisions/memory_chr22_new_sims.pdf")
-ggroups + geom_point(aes(x=as.factor(nanc), y =mem, colour =as.factor(nanc))) + 
+f2<-ggroups + geom_point(aes(x=as.factor(nanc), y =mem, colour =as.factor(nanc))) + 
   facet_wrap(~software) +
   theme_bw(15) +
  labs(colour = "Number of \nAncestral \nPopulations" ) +
   xlab("Number of Ancestral Populations") +
-  ylab("Maximum Memory Usage (Mb)") 
+  ylab("Maximum Memory Usage (MB)") +
+  theme(legend.position = "none")
+tag_facet2(f2)
 dev.off()
 
 for (i in c("RFMix","ELAI","LAMPLD","MOSAIC","Loter")){
--- figures/plot_TPCR_accuracy.R
+++ figures/plot_TPCR_accuracy.R
@@ -4,6 +4,19 @@ library(data.table)
 library(multcompView)
 library(ggplot2)
 library(viridis)
+library(tibble)
+library(tidyr)
+library(egg)
+
+tag_facet2 <- function(p, open = "", close = "", tag_pool = LETTERS, x = -Inf, y = Inf, ## from stackoverflow https://stackoverflow.com/questions/56064042/using-facet-tags-and-strip-labels-together-in-ggplot2 based on egg tag_facet
+                       hjust = -0.5, vjust = 1.5, fontface = 2, family = "", ...) {
+  
+  gb <- ggplot_build(p)
+  lay <- gb$layout$layout
+  tags <- cbind(lay, label = paste0(open, tag_pool[lay$PANEL], close), x = x, y = y)
+  p + geom_text(data = tags, aes_string(x = "x", y = "y", label = "label"), ..., hjust = hjust, 
+                vjust = vjust, fontface = fontface, family = family, inherit.aes = FALSE)
+}
 
 '%&%' = function(a,b) paste(a,b,sep="")
 
@@ -55,7 +68,19 @@ all_software<-bind_rows(RFMix,ELAI,LAMPLD,Loter,MOSAIC)
 all_software_long<-melt(data = all_software, id.vars=c("pop","software","chr")) %>% 
   mutate(value=if_else(pop=="3WAY",value*3/2,value)) %>% 
   mutate(TCR=if_else(chr==22,1-value/(2*50000),1-value/(2*44332)),ID=software %&% "_" %&% chr)
-for (i in c("3WAY","HIS","AFA")){
+
+sub<-all_software_long[(all_software_long$pop== "3WAY"),]
+medians<-tapply(sub$TCR,sub$software,median)
+model<-lm(TCR~software,data=sub)
+print(medians)
+A<-aov(model)
+tukey<-TukeyHSD(A)
+heat_data<-tukey$software %>% 
+  as.data.frame() %>% 
+  rename_all(function(n){paste("3WAY",n,sep="_")}) %>% 
+  rownames_to_column()
+
+for (i in c("HIS","AFA","3WAY")){
   sub<-all_software_long[(all_software_long$pop== i),]
   medians<-tapply(sub$TCR,sub$software,median)
   model<-lm(TCR~software,data=sub)
@@ -63,21 +88,46 @@ for (i in c("3WAY","HIS","AFA")){
   A<-aov(model)
   tukey<-TukeyHSD(A)
   print(tukey$software)
+  heat_data<-tukey$software %>% 
+    as.data.frame() %>% 
+    rename_all(function(n){paste(i,n,sep="_")}) %>% 
+    rownames_to_column() %>%
+    inner_join(heat_data,by="rowname")
+  
   #setEPS()
   #postscript("Z:/data/Local_ancestry_project/revisions/" %&% i %&% "_Tukey.eps")
-  png("Z:/data/Local_ancestry_project/revisions/TPCR_" %&% i %&% "_pooled_Tukey.png")
-  par(mar=c(5,8,4,1)+.1)
-  plot(tukey, las=1)
-  title("\n" %&% i)
-  dev.off()
+  #png("Z:/data/Local_ancestry_project/revisions/TPCR_" %&% i %&% "_pooled_Tukey.png")
+  #par(mar=c(5,8,4,1)+.1)
+  #plot(tukey, las=1)
+  #title("\n" %&% i)
+  #dev.off()
 } #; tukey$software
-tukey$software[]
+heat_data1<-heat_data %>% 
+  select(contains("rowname") | contains("p adj")) %>% 
+  melt(id.vars="rowname") %>%
+  separate(variable,into=c("pop"),sep="_") %>% 
+  rename(p_adj="value")
+heat_data2<-heat_data %>% 
+  select(contains("rowname") | contains("diff")) %>% 
+  melt(id.vars="rowname") %>%
+  separate(variable,into=c("pop"),sep="_") %>% 
+  rename(diff="value")
+heat_data_long<-inner_join(heat_data1,heat_data2,by=c("rowname","pop")) %>% 
+  separate(rowname,into=c("software1","software2"),sep="-",remove=FALSE)
+
+heat_data_matrix<-heat_data_long %>% 
+  filter(pop == "3WAY") %>% dcast(software1 ~ software2, value.var="p_adj") %>% 
+  as.matrix()
+heatmap(heat_data_matrix)
+gH<-ggplot(data=heat_data_long)
+gH + geom_tile(x=)
+
 gV<-ggplot(data = all_software_long, aes(x=ID,y=TCR))
 
-# setEPS()
- postscript("Z:/data/Local_ancestry_project/revisions/TPCR_accuracy_chr22.eps")
-# pdf("Z:/data/Local_ancestry_project/revisions/TPCR_accuracy_both_chr_2.pdf")
-gV + geom_violin(aes(fill=ID),scale = "width") + 
+
+pdf("Z:/data/Local_ancestry_project/revisions/TPCR_accuracy_both_chr_v2.pdf")
+f6<-gV + 
+  geom_violin(aes(fill=ID),scale = "width") + 
   facet_wrap(~pop,ncol=1) + 
   scale_fill_viridis(discrete = T) + 
   geom_boxplot(width=0.1) + 
@@ -85,4 +135,5 @@ gV + geom_violin(aes(fill=ID),scale = "width") +
   theme(axis.text.x = element_text(size=8),legend.position = "none") +
   ylab("True Positive Call Rate") +
   xlab("Software")
+tag_facet2(f6,vjust=14.5)
 dev.off()
